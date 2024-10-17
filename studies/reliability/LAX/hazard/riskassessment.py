ab476d1566806266482bad52c2d96963439042e9
DevasmitDutta <dd17@iitbbs.ac.in>
2024-10-16 21:12:44 -0700

    new folder

--- studies/reliability/LAX/hazard/riskassessment.py
+++ studies/reliability/LAX/hazard/riskassessment.py
@@ -36,7 +36,7 @@ f5, sorted_data_5 = ecdf(x12hr)
 f6, sorted_data_6 = ecdf(x24hr)
 
 # Define the list of window sizes (moving window sizes)
-window_sizes = ['2yr', '5yr', '10yr', '25yr', '50yr', '100yr', '200yr', '500yr', '1000yr'] #np.arange(5,61,1) #[5, 8, 10, 15, 20, 30, 40, 50, 60]  # Generates values from 1 to 10 with a step size of 1
+window_sizes = np.arange(5,61,1) #[10, 25, 50, 60] #['2yr', '5yr', '10yr', '25yr', '50yr', '100yr', '200yr', '500yr', '1000yr'] #np.arange(5,61,1) #[5, 8, 10, 15, 20, 30, 40, 50, 60]  # Generates values from 1 to 10 with a step size of 1
 line_styles = ['-', '--', ':', '-.', '.-.']  # Different line styles for each window size
 
 columns = ['x1hr', 'x2hr', 'x3hr', 'x6hr', 'x12hr', 'x24hr']
@@ -49,65 +49,52 @@ df_collect_pf_exceedance = pd.DataFrame(index=window_sizes, columns=columns)
 colors = ['r', 'g', 'b', 'c', 'm', 'k']  # Red, Green, Blue, Cyan, Magenta, Black
 
 # Create figure to plot everything
-plt.figure(figsize=(10, 6))
-# plt.scatter(sorted_data_1, f1, marker='^', color='r', label='Observed - 1 hr')
-# plt.scatter(sorted_data_2, f2, marker='^', color='g', label='Observed - 2 hr')
-# plt.scatter(sorted_data_3, f3, marker='^', color='b', label='Observed - 3 hr')
-# plt.scatter(sorted_data_4, f4, marker='^', color='c', label='Observed - 6 hr')
-# plt.scatter(sorted_data_5, f5, marker='^', color='m', label='Observed - 12 hr')
-# plt.scatter(sorted_data_6, f6, marker='^', color='k', label='Observed - 24 hr')
+# plt.figure(figsize=(10, 6))
 
 # Define the function to fit and plot GEV
 def duration_window_wise(window_size, duration):
-    # if duration != 'x24hr':
-    #   max_index = len(x1hr)
-    #   gev_params = []
-    # else:
-    #   max_index = len(x24hr)
-    #   gev_params = []
+    if duration != 'x24hr':
+      max_index = len(x1hr)
+      gev_params = []
+    else:
+      max_index = len(x24hr)
+      gev_params = []
     
-    # data = intensity_data[duration].dropna()
+    data = intensity_data[duration].dropna()
 
-    # for i in range(max_index - window_size + 1):
-    #     window_data = data.iloc[i:i + window_size]
+    for i in range(max_index - window_size + 1):
+        window_data = data.iloc[i:i + window_size]
 
-    #     params_window = gev.fit(window_data)
+        params_window = gev.fit(window_data)
 
-    #     gev_params.append(params_window)
+        gev_params.append(params_window)
 
-    # x = np.linspace(0, max(np.max(sorted_data_1), np.max(sorted_data_2), np.max(sorted_data_3),
-    #                        np.max(sorted_data_4), np.max(sorted_data_5), np.max(sorted_data_6)), 1000)
+    x = np.linspace(0, max(np.max(sorted_data_1), np.max(sorted_data_2), np.max(sorted_data_3),
+                           np.max(sorted_data_4), np.max(sorted_data_5), np.max(sorted_data_6)), 1000)
 
-    # mean_gev_params = np.mean(gev_params, axis=0)
+    mean_gev_params = np.mean(gev_params, axis=0)
         # Assuming mean_gev_params_1 contains the shape, location, and scale parameters
     
-    # shape, loc, scale = mean_gev_params
+    shape, loc, scale = mean_gev_params
 
     num_samples = 1000
     # Generate 100 random samples from the GEV distribution
-    # random_samples = np.sort(stats.genextreme.rvs(c=shape, loc=loc, scale=scale, size=num_samples))
+    random_samples = np.sort(stats.genextreme.rvs(c=shape, loc=loc, scale=scale, size=num_samples))
     # print(f'Random samples: {random_samples}')
 
-    mean_data = pd.read_csv('studies/reliability/LAX/mean_intensity_conf_interval.csv')
-    _95thpercentile_data = pd.read_csv('studies/reliability/LAX/95_percent_intensity_conf_interval.csv')
-    # print(_95thpercentile_data)
+    # mean_data = pd.read_csv('studies/reliability/LAX/mean_intensity_conf_interval.csv')
+    # _95thpercentile_data = pd.read_csv('studies/reliability/LAX/95_percent_intensity_conf_interval.csv')
 
-    # print(mean_data.iloc[1:,1])
-    # Clean the DataFrame by removing unnecessary columns
+    # mean_data.set_index('Dur (hr)', inplace=True)
+    # mean = mean_data.loc[duration, window_size]
 
-    # Set the 'Dur (hr)' column as the index
-    mean_data.set_index('Dur (hr)', inplace=True)
-    mean = mean_data.loc[duration, window_size]
-    # print(mean)
-
-    _95thpercentile_data.set_index('Dur (hr)', inplace=True)
-    # print(_95thpercentile_data)
-    percentile_95 = _95thpercentile_data.loc[duration, window_size]
+    # _95thpercentile_data.set_index('Dur (hr)', inplace=True)
+    # percentile_95 = _95thpercentile_data.loc[duration, window_size]
     
-    gamma = 0.57721
-    beta = (mean - percentile_95) / (np.log(-np.log(0.95)) + gamma)
-    mu = mean - beta * gamma
-    random_samples = np.sort(gumbel.rvs(loc=mu, scale=beta, size=num_samples))
+    # gamma = 0.57721
+    # beta = (mean - percentile_95) / (np.log(-np.log(0.95)) + gamma)
+    # mu = mean - beta * gamma
+    # random_samples = np.sort(gumbel.rvs(loc=mu, scale=beta, size=num_samples))
     
     Pf = np.zeros(num_samples)
     threshold_num = np.random.uniform(0, 1, num_samples)
@@ -134,7 +121,7 @@ def duration_window_wise(window_size, duration):
         else:
             print('False')
 
-    # print(f'shape: {mean_gev_params[0]}, loc: {mean_gev_params[1]}, scale: {mean_gev_params[2]}')
+    # print(f'shape: {mean_gev_params[0]}, loc: {mean_gev_params[1]}, scale: {mean_gev_params[2]} for {duration} and {window_size} years')
 
     # plt.hist(threshold_num, bins = 100, edgecolor='black', alpha=0.7)
     # plt.title('Histogram of Uniform Samples')
@@ -142,8 +129,9 @@ def duration_window_wise(window_size, duration):
     # plt.ylabel('Frequency')
     # plt.show()
 
-    # plt.hist(random_samples, edgecolor='black', alpha=0.7, label=f'{window_size} years')
-    # plt.show()
+    # plt.hist(random_samples, edgecolor='black', bins = 100, alpha=0.7, label=f'{window_size} yrs, shape: {mean_gev_params[0]:.2f}, loc: {mean_gev_params[1]:.2f}, scale: {mean_gev_params[2]:.2f}')
+    # plt.hist(random_samples, edgecolor='black', bins = 100, alpha=0.7, label=f'{window_size} yrs, shape: {0:.2f}, loc: {mu:.2f}, scale: {beta:.2f}')
+
 
     Prob_of_exceedance = cnt/num_samples
     print(f'Probability of exceedance: {Prob_of_exceedance} in {window_size} years over {duration} duration') 
@@ -155,12 +143,13 @@ for (i, window_size) in enumerate(window_sizes):
     for (j, duration) in enumerate(columns):
         duration_window_wise(window_size, duration)
 
-# plt.title('Generated random samples from Gumbel (GEV-I) distribution for x1hr')
+# plt.title('Generated random samples from GEV distribution for x1hr')
 # plt.xlabel('Intensity Measure')
 # plt.ylabel('Frequency')
 # plt.legend()
-# plt.show()
+# plt.savefig('studies/reliability/LAX/Fragility_fit/Random_samples_from_GEV_distribution_x1hr.png')
 
+plt.figure(figsize=(10, 6))
 # Finalize figure with labels and title
 for (i, duration) in enumerate(columns):
     plt.plot(window_sizes, df_collect_pf_exceedance[duration], label = f'{duration}', color = colors[i])
