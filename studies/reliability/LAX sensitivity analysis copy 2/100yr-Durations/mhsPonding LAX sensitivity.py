81769e68bf2cd7b7e039df2a1ebbc5c5bc6388dc
DevasmitDutta <dd17@iitbbs.ac.in>
2024-12-03 17:02:55 -0800

    fix unit issue

---
+++ studies/reliability/LAX sensitivity analysis copy 2/100yr-Durations/mhsPonding LAX sensitivity.py
@@ -0,0 +1,7 @@
+
+# run_analysis('W14X22',360.0,0.020833333333333332,6.944444444444444e-05,1976)
+# print(360 == 30.0 * 12, 0.020833333333333332 == 0.25/12, 6.944444444444444e-05 == 10*((1/1000)/(12**2)), 1976 == 1976)
+
+exec(open('/home/devasmit/Desktop/PyPonding/studies/reliability/LAX sensitivity analysis copy 2/100yr-Durations/fragility_assessment LAX sensitivity.py').read())
+run_analysis_multiple_periods('W14X22',360.0,0.020833333333333332,6.944444444444444e-05,1976)
+# run_analysis_multiple_periods('W10X12',360.0,0.020833333333333332,6.944444444444444e-05,1976)
